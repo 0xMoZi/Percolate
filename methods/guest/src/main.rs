65b4a845c61b88d6d87fb684063a71be4cc413f3
0xMoZi <0x0fairuz@gmail.com>
2026-09-08 01:59:57 +0700

    feat(guest): implement zkVM guest entrypoint

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,3 +1,17 @@
+#![no_main]
+
+use alloy_sol_types::SolValue;
+use percolate_core::{PrivateInput, PublicInput, compute_journal};
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
 fn main() {
-    println!("Hello, world!");
+    let priv_in: PrivateInput = env::read();
+    let pub_in: PublicInput = env::read();
+
+    let journal = compute_journal(&pub_in, &priv_in)
+        .unwrap_or_else(|e| panic!("Percolate guest: failed: {:?}", e));
+
+    env::commit_slice(&*journal.abi_encode().as_slice());
 }
